ff4e3b60d3c924fc1c21dfd9df9d7a16baf712ef
Kellen Presta <kpresta04@gmail.com>
2019-12-13 10:46:40 -0600

    prompts created

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # password-generator
+
+The page prompts the user for input and generates a password based on their responses. Also includes a button to copy the password to their clipboard.
+
+kpresta04.github.io/password-generator
--- script.js
+++ script.js
@@ -2,22 +2,64 @@
 var generateBtn = document.querySelector("#generateButton");
 var copyBtn = document.querySelector("#copyButton");
 
+var userChoice = {
+  pwLength: null,
+  specialChar: false,
+  numericChar: false,
+  lowerCase: true,
+  upperCase: false
+};
+
+function userPrompts() {
+  userResponse = prompt("Enter password length from 8-128");
+
+  userChoice.pwLength = parseInt(userResponse);
+
+  console.log(userChoice.pwLength);
+  console.log(typeof userChoice.pwLength);
+  userResponse = confirm("Use special characters?");
+  userChoice.specialChar = userResponse;
+
+  userResponse = confirm("Use numeric characters?");
+  userChoice.numericChar = userResponse;
+
+  userResponse = confirm(
+    "Mix lower and uppercase? Click cancel for lowercase only"
+  );
+  if (userResponse) {
+    userChoice.upperCase = true;
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  userChoice.pwLength = "hello";
+  console.log(userChoice.pwLength);
+  //   var password = generatePassword();
+  //   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //   passwordText.value = password;
 
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+  //   copyBtn.removeAttribute("disabled");
+  //   copyBtn.focus();
 }
 
 function copyToClipboard() {
-  // BONUS
+  var copyText = document.getElementById("pgenerator");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied to clipboard");
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", userPrompts);
 
 // BONUS EVENT LISTENER
+copyBtn.addEventListener("click", copyToClipboard);
