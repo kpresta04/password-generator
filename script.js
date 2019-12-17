a9bb018a4127b61e3d6490f4c12051234d7ef7c0
Kellen Presta <kpresta04@gmail.com>
2019-12-17 11:51:55 -0600

    added input validation

--- script.js
+++ script.js
@@ -23,38 +23,57 @@ function genPassword() {
 }
 
 function userPrompts() {
+  //A series a prompts to get criteria for the password
   userResponse = prompt("Enter password length from 8-128");
-
-  userChoice.pwLength = parseInt(userResponse);
-
-  userResponse = confirm("Use special characters?");
+  console.log(typeof userResponse);
   console.log(userResponse);
 
-  if (userResponse) {
-    userChoice.pwdChars.push("!@#$&*");
+  if (userResponse === null) {
+    //pass because user pressed Cancel
+    //
+  } else {
+    while (
+      //Verify user entered a valid number
+
+      isNaN(Number(userResponse)) ||
+      Number(userResponse) < 8 ||
+      Number(userResponse) > 128
+    ) {
+      userResponse = prompt(
+        "Invalid input. Enter a number from 8-128 for password length"
+      );
+    }
+
+    userChoice.pwLength = parseInt(userResponse);
+
+    userResponse = confirm("Use special characters?");
+    console.log(userResponse);
+
+    if (userResponse) {
+      userChoice.pwdChars.push("!@#$&*");
+    }
+
+    userResponse = confirm("Use numeric characters?");
+    console.log(userResponse);
+
+    if (userResponse) {
+      userChoice.pwdChars.push("0123456789");
+    }
+
+    userResponse = confirm(
+      "Mix lower and uppercase? Click cancel for lowercase only"
+    );
+    console.log(userResponse);
+
+    if (userResponse) {
+      userChoice.pwdChars.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+    console.log(userChoice.pwdChars);
+    // pwgen = randPassword(userChoice.pwLength);
+    userChoice.pwdChars = userChoice.pwdChars.join("");
+    genPassword();
   }
-
-  userResponse = confirm("Use numeric characters?");
-  console.log(userResponse);
-
-  if (userResponse) {
-    userChoice.pwdChars.push("0123456789");
-  }
-
-  userResponse = confirm(
-    "Mix lower and uppercase? Click cancel for lowercase only"
-  );
-  console.log(userResponse);
-
-  if (userResponse) {
-    userChoice.pwdChars.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-  }
-  console.log(userChoice.pwdChars);
-  // pwgen = randPassword(userChoice.pwLength);
-  userChoice.pwdChars = userChoice.pwdChars.join("");
-  genPassword();
 }
-
 // Write password to the #password input
 function writePassword(password) {
   var passwordText = document.querySelector("#pgenerator");
