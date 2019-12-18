df9e14ccf8edf208c317d16f09b8cc8c1e8ad6a2
Kellen Presta <kpresta04@gmail.com>
2019-12-18 11:27:19 -0600

    functionality working

--- script.js
+++ script.js
@@ -17,22 +17,7 @@ const randomFunc = {
   number: getRandomNumber,
   symbol: getRandomSymbol
 };
-
-copyBtn.addEventListener("click", () => {
-  const textarea = document.createElement("textarea");
-  const password = resultEl.innerText;
-
-  if (!password) {
-    return;
-  }
-
-  textarea.value = password;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand("copy");
-  textarea.remove();
-  alert("Password copied to clipboard");
-});
+copyBtn.addEventListener("click", copyToClipboard);
 
 generateBtn.addEventListener("click", () => {
   const length = +lengthEl.value;
@@ -111,106 +96,13 @@ close_btn.addEventListener("click", () => {
   social_panel_container.classList.remove("visible");
 });
 
-function genPassword() {
-  var pwdChars = userChoice.pwdChars;
-  var pwdLen = userChoice.pwLength;
-  var pwgen = Array(pwdLen)
-    .fill(pwdChars)
-    .map(function(x) {
-      return x[Math.floor(Math.random() * x.length)];
-    })
-    .join("");
-  console.log(pwgen);
-  console.log(pwgen.length);
-  var valid = false;
-  var lowerSub = userChoice.lowerChars.split("");
-  var numberSub = userChoice.numberChars.split("");
-  var specialSub = ["!", "@", "#", "$", "&", "*"];
-  var upperSub = userChoice.upperChars.split("");
-
-  console.log(lowerSub);
-  console.log(pwChecker(pwgen, lowerSub));
-
-  pwChecker(pwgen, numberSub);
-  pwChecker(pwgen, specialSub);
-  pwChecker(pwgen, upperSub);
-
-  // var specialSub = ["!", "@", "#", "$", "&", "*"],
-  //   length = specialSub.length;
-  // while (length--) {
-  //   if (pwgen.indexOf(specialSub[length]) != -1) {
-  //     console.log("Pw contains special characters");
-  //     valid = true;
-  //   }
-  // }
-  writePassword(pwgen);
-  userChoice.pwdChars = ["abcdefghijklmnopqrstuvwxyz"];
-}
-
-function userPrompts() {
-  //A series a prompts to get criteria for the password
-  userResponse = prompt("Enter password length from 8-128");
-  console.log(typeof userResponse);
-  console.log(userResponse);
-
-  if (userResponse === null) {
-    //pass because user pressed Cancel
-    //
-  } else {
-    while (
-      //Verify user entered a valid number
-
-      isNaN(Number(userResponse)) ||
-      Number(userResponse) < 8 ||
-      Number(userResponse) > 128
-    ) {
-      userResponse = prompt(
-        "Invalid input. Enter a number from 8-128 for password length"
-      );
-    }
-
-    userChoice.pwLength = parseInt(userResponse);
-
-    userResponse = confirm("Use special characters?");
-    console.log(userResponse);
-
-    if (userResponse) {
-      userChoice.pwdChars.push("!@#$&*");
-    }
-
-    userResponse = confirm("Use numeric characters?");
-    console.log(userResponse);
-
-    if (userResponse) {
-      userChoice.pwdChars.push("0123456789");
-    }
-
-    userResponse = confirm(
-      "Mix lower and uppercase? Click cancel for lowercase only"
-    );
-    console.log(userResponse);
-
-    if (userResponse) {
-      userChoice.pwdChars.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    }
-    // console.log(userChoice.pwdChars[0].split(""));
-    // pwgen = randPassword(userChoice.pwLength);
-    userChoice.pwdChars = userChoice.pwdChars.join("");
-    genPassword();
-  }
-}
-// Write password to the #password input
-function writePassword(password) {
-  var passwordText = document.querySelector("#pgenerator");
-
-  passwordText.value = password;
-
-  // copyBtn.removeAttribute("disabled");
-  // copyBtn.focus();
-}
-
 function copyToClipboard() {
-  var copyText = document.getElementById("pgenerator");
+  var copyText = document.getElementById("result");
+  const password = copyText.textContent;
+
+  if (!password) {
+    return;
+  }
 
   /* Select the text field */
   copyText.select();
